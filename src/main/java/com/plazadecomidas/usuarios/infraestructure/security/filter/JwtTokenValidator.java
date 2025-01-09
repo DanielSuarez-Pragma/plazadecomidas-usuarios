2c6f75553fc1df05b390081ee6848213f41ca81b
Daniel Suarez <daniel.suarez@pragma.com.co>
2025-01-09 16:18:37 -0500

    feat(security): implement JWT authentication and authorization
    - Added JWT-based authentication and authorization mechanisms.
    - Configured Spring Security with stateful session management.
    - Implemented `UserDetailServiceImpl` to fetch user details and assign roles dynamically.
    - Added custom filters for JWT validation.
    - Included token generation and validation utilities.
    - Secured endpoints with role-based access control.
    
    This implementation enhances the security layer, providing role-specific access to the API.

--- src/main/java/com/plazadecomidas/usuarios/infraestructure/security/filter/JwtTokenValidator.java
+++ src/main/java/com/plazadecomidas/usuarios/infraestructure/security/filter/JwtTokenValidator.java
@@ -24,7 +24,7 @@ public class JwtTokenValidator extends OncePerRequestFilter {
 
     private JwtUtils jwtUtils;
 
-    public jwtTokenValidator(JwtUtils jwtUtils) {
+    public JwtTokenValidator(JwtUtils jwtUtils) {
         this.jwtUtils = jwtUtils;
     }
 
